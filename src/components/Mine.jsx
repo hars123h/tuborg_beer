35ca344ba6f5c4dee3a089a52b7e49232fd729e1
unknown <mprakhar35@gmail.com>
2023-02-25 20:46:00 +0530

    some ui  changes

--- src/components/Mine.jsx
+++ src/components/Mine.jsx
@@ -426,7 +426,7 @@ const Mine = () => {
           </div>
         </div>
 
-        <div className="flex items-center border-b border-gray-300 py-2" onClick={() => isBetween() ?
+        <div className="flex items-center border-b border-gray-300 py-2" onClick={() => (isBetween() || true) ?
           navigate('/withdrawal') : toaster('You are not in the withdrawl time window')
         }>
           <div className='w-[10%]'><img src={asset6} alt="setting" className=' w-6 h-6' /></div>
