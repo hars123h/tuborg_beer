560dad0eecf1391bd93756a2ca0b313fb06127cd
unknown <mprakhar35@gmail.com>
2023-02-25 22:06:35 +0530

    some ui  changes

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -58,21 +58,21 @@ const ForgotPassword = () => {
                     <div>{toasterText}</div>
                 </div>
             </div>:null}
-            <div className='text-center bg-red-600 font-sans text-white pt-2 text-lg mb-10
+            <div className='text-center bg-red-800 font-sans text-white pt-2 text-lg mb-10
         pb-2'> <svg xmlns="http://www.w3.org/2000/svg" onClick={() => navigate('/login')} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute left-2 cursor-pointer hover:bg-white hover:stroke-black hover:rounded-full transition rounded-full ease-in-out delay-150 duration-200">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
                 Find Password</div>
                 {/* [#d3d6fe] */}
-            <div className="box mb-20 border-2 m-auto border-gray-200 bg-red-300 rounded-3xl border-solid lg:w-3/5 w-4/5 shadow-xl shadow-red-600 p-4 w-50% flex flex-col">
+            <div className="box mb-20 border-2 m-auto border-gray-200 bg-blue-200 rounded-3xl border-solid lg:w-3/5 w-[86%] shadow-xl shadow-red-600 p-4 w-50% flex flex-col">
                 <div className="no_phone mb-3 flex items-center bg-white border-2 border-gray-100 rounded-full ">
                     <input onChange={(e) => { setMobno(e.target.value); setOTPfield(String(Math.floor(100000 + Math.random() * 900000))) }} type="number" className='p-2 w-full outline-none rounded-full' placeholder='Phone number' name="phoneno" id="phoneno" />
-                    <div onClick={handleMessage} className='opt w-10 bg-red-800 mr-4 text-xs cursor-pointer p-2 shadow-md rounded-2xl text-white text-center'>OTP</div>
+                    <div onClick={handleMessage} className='opt w-20 bg-red-800 mr-4 text-xs cursor-pointer  shadow-md rounded-md text-white text-center'>OTP</div>
                 </div>
 
 
                 <input type="password" className='p-2 mb-3 outline-none border-2 border-gray-100 rounded-full' onChange={(e) => setOtp(e.target.value)} placeholder='Please enter the OTP' name="passowrd" id="pass" />
-                <button onClick={handleReset} disabled={!(otpfield == otp && otp.length > 0 && otpfield.length > 0)} className={`${(otpfield == otp && otp.length > 0 && otpfield.length > 0) ? 'bg-red-500' : 'bg-gray-500 cursor-not-allowed'} text-white pt-1 pb-1 rounded-full text-lg`}>Confirm</button>
+                <button onClick={handleReset} disabled={!(otpfield == otp && otp.length > 0 && otpfield.length > 0)} className={`${(otpfield == otp && otp.length > 0 && otpfield.length > 0) ? 'bg-red-800' : 'bg-gray-500 cursor-not-allowed'} text-white pt-1 pb-1 rounded-full text-lg`}>Confirm</button>
                 <div onClick={() => { navigate('/login') }} className='cursor-pointer text-center bg-white text-black mt-2 p-2 mb-2 border-2 border-gray-100 rounded-full'>
                     Already have an account, log in
                 </div>
