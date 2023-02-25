be82278cf4593d66fba19b8fde63b405ee52624a
unknown <mprakhar35@gmail.com>
2023-02-25 19:07:01 +0530

    some ui  changes

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import amaz_slide1 from '../images/amaz_slide1.jpg'
-import amaz_slide2 from '../images/amaz_slide2.jpg'
-import amaz_slide3 from '../images/amaz_slide3.jpg'
-import asset14 from '../images/assets2/asset14.jpg';
-import tuborg_logo from '../images/tuborg_logo.svg';
+import tuborg_slide1 from '../images/tuborg_slide1.jpg';
+import tuborg_slide2 from '../images/tuborg_slide2.jpg';
+import tuborg_slide3 from '../images/tuborg_slide3.jpg';
 
 
 const Slider = () => {
@@ -13,14 +11,14 @@ const Slider = () => {
     <div className='sm:w-3/5 lg:w-3/5 mx-auto '>
       <Carousel showThumbs={false} autoPlay showArrows={true} infiniteLoop>
         <div>
-          <img src={tuborg_logo} className="h-[200px]" alt="img_2" />
+          <img src={tuborg_slide1} className="h-[200px]" alt="img_2" />
         </div>
         <div>
-          <img src={tuborg_logo} className="h-[200px]" alt="img_2" />
+          <img src={tuborg_slide2} className="h-[200px]" alt="img_2" />
         </div>
 
         <div>
-          <img src={tuborg_logo} className="h-[200px]" alt="img_1" />
+          <img src={tuborg_slide3} className="h-[200px]" alt="img_1" />
         </div>
 
       </Carousel>
